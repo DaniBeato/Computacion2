e222daa9b5a235351236ba8ef743fa030c350a14
Daniel <danielbeatoramirez@gmail.com>
2021-09-27 22:50:37 -0300

    multiprocessing_pipe

---
+++ ejer9(multiprocessing_pipe).py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import multiprocessing, os
+
+
+def hijo1(emisor):
+    msg = input('Ingrese algo...')
+    emisor.send(msg)
+
+
+def hijo2(receptor):
+     #while True:
+        msg = receptor.recv()
+        print('Leyendo (pid: ', os.getpid(), '):', msg)
+
+
+if __name__ == '__main__':
+        emisor, receptor = multiprocessing.Pipe()
+        child1 = multiprocessing.Process(target = hijo1, args=(emisor,))
+        child2 = multiprocessing.Process(target = hijo2, args=(receptor,))
+        while True:
+            child1.run()
+            child2.run()
+
+
+
+
+
