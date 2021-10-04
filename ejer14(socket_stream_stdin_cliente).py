b253a29af73d33935423ef2e39a5796cadb9a8c5
Daniel <danielbeatoramirez@gmail.com>
2021-10-03 23:58:01 -0300

    socket_stdin

---
+++ ejer14(socket_stream_stdin_cliente).py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+import sys, socket, getopt
+
+if __name__=='__main__':
+    (opt, arg) = getopt.getopt(sys.argv[1:], 'p:t:f:')
+    for (op,ar) in opt:
+        if op == '-a':
+            host = int(ar)
+        if op == '-p':
+            puerto = int(ar)
+        if op == '-t':
+            protocolo = int(ar)
+
+    if protocolo == 'tcp':
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, puerto))
+        while True:
+            try:
+                msg = input('Ingrese el mensaje a enviar: ')
+                s.send(msg.encode('ascii'))
+            except EOFError:
+                s.close()
+                break
+    else:
+        if protocolo == 'udp':
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            while True:
+                try:
+                    msg = input('Ingrese el mensaje a enviar: ').encode()
+                    s.sendto(msg, (host, puerto))
+                except EOFError:
+                    s.sendto("".encode(), (host, puerto))
+                    s.close()
+                    break
+
+
+
+
+
+
---
+++ ejer14(socket_stream_stdin_servidor).py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+import sys, socket, getopt
+
+if __name__=='__main__':
+    (opt, arg) = getopt.getopt(sys.argv[1:], 'p:t:f:')
+    for (op,ar) in opt:
+        if op == '-p':
+            puerto = int(ar)
+        if op == '-t':
+            protocolo = int(ar)
+        if op == '-f':
+            ruta = int(ar)
+
+    if protocolo == 'tcp':
+        serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        host = socket.gethostname()
+        serversocket.bind((host, puerto))
+        serversocket.listen()
+        print("Esperando conexiones")
+        clientsocket, addr = serversocket.accept()
+        while True:
+            f = open(ruta, 'a')
+            d = clientsocket.recv(1024)
+            msg = d.decode('ascii')
+            f.write(msg + '\n')
+            print('Dirección:', addr)
+            print('Mensaje Recibido: ', msg)
+    else:
+        if protocolo == 'udp':
+            serversocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            host = socket.gethostname()
+            serversocket.bind((host, puerto))
+            print("Esperando conexiones")
+            while True:
+                f = open(ruta, 'a')
+                d, addr = serversocket.recvfrom(1024)
+                msg = d.decode('ascii')
+                f.write(msg + "\n")
+                direccion = addr[0]
+                puerto = addr[1]
+
+                print('Dirección:', addr, 'Puerto: ', puerto)
+                print('Mensaje Recibido: ', msg)
+
+
+
+
+
+
+
+
+
+
+
+
